6b78fa28294b46165444af6130197b12dde627b6
GilPOP <wolfye@naver.com>
2021-10-09 09:30:01 +0900

    offer , answer , icecandidate , addstream finish

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -5,9 +5,15 @@ const muteBtn = document.getElementById("mute");
 const cameraBtn = document.getElementById("camera");
 const camerasSelect = document.getElementById("cameras");
 
+const call = document.getElementById("call");
+
+call.hidden = true;
+
 let myStream;
 let muted = false; // 처음엔 음소거가 아님
 let cameraOff = false; // 처음엔 카메라가 켜져 있음
+let roomName; // 방의 이름을 공유하기 위함
+let myPeerConnection; // 누구와 연결되었는지
 
 async function getCameras() {
   // 카메라의 정보 , 변경 등을 할 수 있게 구현함
@@ -50,7 +56,7 @@ async function getMedia(deviceId) {
       //사용자에게 미디어 입력 장치 사용 권한을 요청하며, 사용자가 수락하면 요청한 미디어 종류의 트랙을 포함한 MediaStream (en-US)을 반환
       deviceId ? userConstrains : defaultConstrains // 디바이스 아이디가 있다면 user가 설정한 카메라를 실행 없다면 초기값 카메라 실행
     );
-    //console.log(myStream);
+    console.log(myStream);
     myFace.srcObject = myStream; // srcObject란 현재 HTMLMediaElement 객체에서 재생중이거나 재생되었던 미디어를 참조하는 MediaStream 객체를 참조한다.
     if (!deviceId) {
       // 디바이스 아이디가 없다면 getCameras 함수 실행
@@ -60,8 +66,6 @@ async function getMedia(deviceId) {
     console.log(e);
   }
 }
-
-getMedia();
 
 function handleMuteClick() {
   myStream.getAudioTracks().forEach((track) => {
@@ -98,3 +102,96 @@ async function handleCameraChange() {
 muteBtn.addEventListener("click", handleMuteClick);
 cameraBtn.addEventListener("click", handleCameraClick);
 camerasSelect.addEventListener("input", handleCameraChange);
+
+// welcome Form (join room) // 방 입장에 관한 코드들
+
+const welcome = document.getElementById("welcome");
+const welcomeForm = welcome.querySelector("form");
+
+async function initCall() {
+  welcome.hidden = true;
+  call.hidden = false;
+  await getMedia();
+  makeConnection(); // 서버로 보냄
+}
+
+async function handleWelcomeSubmit(event) {
+  event.preventDefault(); // 기존이벤트 초기화
+  const input = welcomeForm.querySelector("input");
+  //console.log(input.value);
+  await initCall(); // myPeerConnection 생성에 서버측의 속도를 맞추기 위함
+  socket.emit("join_room", input.value); // 백앤드로 join_room 에 대한 이벤트와 인자 , 실행할 함수를 보냄
+  roomName = input.value;
+  input.value = "";
+}
+
+welcomeForm.addEventListener("submit", handleWelcomeSubmit);
+
+// socket code socket io 이벤트에 관한 코드
+
+socket.on("welcome", async () => {
+  // Peer A가 offer을 생성하고 연결 인터페이스와 관련이 있는 로컬 설명 (local description)을 변경하고 그 offer을 보내는 과정입니다.
+  try {
+    //console.log("오퍼를 생성");
+    const offer = await myPeerConnection.createOffer(); // RTC 에 필요한 type offer 생성하기
+
+    myPeerConnection.setLocalDescription(offer); // myPeerConnection 에 offer 값 설정하기
+    //console.log(offer);
+    socket.emit("offer", offer, roomName); // offer을 Peer B에게 보냄
+    //console.log("오퍼를 보냄");
+  } catch (e) {
+    console.log(e);
+  }
+});
+
+socket.on("offer", async (offer) => {
+  // Peer B
+  //console.log("오퍼를 받아서 응답을 만듬");
+  myPeerConnection.setRemoteDescription(offer); // 응답받은 offer을 원격 피어의 현재 제공 또는 응답으로 설정합니다.
+  const answer = await myPeerConnection.createAnswer(); // Peer A의 offer 연결으로 인한 값인 type answer을 생성합니다
+  myPeerConnection.setLocalDescription(answer); // myPeerConnection 에 응답받은 offer 값 설정하기
+  //console.log(answer);
+  socket.emit("answer", answer, roomName);
+  //console.log("응답을 보냄");
+});
+
+socket.on("answer", (answer) => {
+  //console.log("응답을 받고 설정함");
+  myPeerConnection.setRemoteDescription(answer); // Peer B 가 보내온 값으로 myPeerConnection 에 원격 피어의 현재 제공 또는 응답으로 설정합니다. remote로 해야함
+});
+
+socket.on("ice", async (ice) => {
+  //console.log("아이스를 받음");
+  //console.log(ice);
+  await myPeerConnection.addIceCandidate(ice).catch((e) => console.log(e)); // WebRTC가 선택한 candidate를 입력받음
+});
+
+// RTC Code 리얼타임코드
+
+function makeConnection() {
+  myPeerConnection = new RTCPeerConnection(); // 브라우저 간 peer to peer 연결을 만듬
+  //console.log("RTC를 만듬");
+  myPeerConnection.addEventListener("icecandidate", handleIce); // 서로 통신연결을 하였을 때 실행됨
+  myPeerConnection.addEventListener("addstream", handleAddStream); // icecandidate 가 검증 끝나면 addstream 이벤트가 시작됨
+  //console.log(myStream);
+  myStream
+    .getTracks()
+    .forEach((track) => myPeerConnection.addTrack(track, myStream)); // 양쪽 브라우저에서 오디오,비디오 데이터 stream을 받아서  연결안에 집어 넣었습니다
+  //console.log("각각의 mystream 생성 완료");
+}
+
+function handleIce(data) {
+  //console.log(data);
+  //console.log("아이스를 보냄");
+  //console.log(data);
+  socket.emit("ice", data.candidate, roomName); // WebRTC가 선택한 candidate를 사용해서 연결 보냄
+}
+
+function handleAddStream(data) {
+  //console.log("비디오를 만들라고함");
+  //console.log(data);
+  //console.log("내 스트림", myStream);
+  //console.log("상대의 스트림", data.stream);
+  const peerFace = document.getElementById("peerFace");
+  peerFace.srcObject = data.stream; // peerFace 비디오 태그에 상대방의 stream을 입력
+}
--- src/server.js
+++ src/server.js
@@ -26,90 +26,24 @@ instrument(wsServer, {
   auth: false,
 }); //어드민 패널에 필요한 요소
 
-function roomCount(roomName) {
-  const count = wsServer.sockets.adapter.rooms.get(roomName)?.size;
-  return count;
-}
-
-function publicRooms() {
-  // 현재 채팅방의 목록을 표현하는 함수임
-  const {
-    sockets: {
-      adapter: { rooms, sids }, // rooms 와 sids 는 공적인 채팅방, 개인적 채팅방의 표현 개수가 다르다는 점을 이용함
-    },
-  } = wsServer;
-  const publicRooms = [];
-  rooms.forEach((value, key) => {
-    if (sids.get(key) === undefined) {
-      // sids.get(key)를 했을 때 undefined 면 개인적 채팅방으로 사용자가 만든 채팅방인걸 알 수 있다.
-      publicRooms.push(key);
-    }
-  });
-  return publicRooms;
-}
-
 wsServer.on("connection", (socket) => {
-  wsServer.sockets.emit("room_change", publicRooms()); // 방에 아무도 없을 때 채팅방 상황이 변경되었다고 알림
-  socket["nickname"] = "Anon";
-  socket.onAny((event) => {
-    //console.log(wsServer.sockets.adapter);
-    console.log(`Socket Event : ${event}`); // socket에 이벤트가 발생하면 출력함
+  //ws 서버, socket.io 를 연결
+  socket.on("join_room", (roomName) => {
+    //프론트에서 받은 이벤트와 입력값으로 방을 만들고 입장함
+    socket.join(roomName);
+    socket.to(roomName).emit("welcome"); // 룸네임에 있는 사용자들에게 welcome 이벤트를 보냄
   });
-  socket.on("enter_room", (roomName, done) => {
-    socket.join(roomName); // 룸에 입장함
-    done(roomCount(roomName)); //프론트에 있는 done()에 해당하는 함수 실행
-    wsServer.to(roomName).emit("welcome", socket.nickname, roomCount(roomName)); // 사용자가 룸에 입장하면 프론트에 닉네임 인자와 welcome 이벤트를 보냄
-    wsServer.sockets.emit("room_change", publicRooms()); // 방을 사용자가 만들면 채팅방 상황이 변경되었디고 알림
+  socket.on("offer", (offer, roomName) => {
+    socket.to(roomName).emit("offer", offer); // Peer B에게로 전송함
   });
-  socket.on("new_message", (msg, roomName, done) => {
-    socket.to(roomName).emit("new_message", `${socket.nickname} : ${msg}`); // room에 해당하는곳에 new_message 와 백엔드에서 받은 msg 인자를 프론트로 보냄
-    done(); //프론트에 있는 done()에 해당하는 함수 실행
+  socket.on("answer", (answer, roomName) => {
+    socket.to(roomName).emit("answer", answer); // Peer A에게로 전송함
   });
-  socket.on("disconnecting", () => {
-    socket.rooms.forEach((room) =>
-      socket.to(room).emit("bye", socket.nickname, roomCount(room) - 1)
-    ); //사용자가 브라우저를 끄거나 인터넷 연결이 끊길 때 프론트에 알림
+  socket.on("ice", (ice, roomName) => {
+    socket.to(roomName).emit("ice", ice);
   });
-  socket.on("disconnect", () => {
-    wsServer.sockets.emit("room_change", publicRooms()); // 방에 아무도 없을 때 채팅방 상황이 변경되었다고 알림
-  });
-  socket.on("nickname", (nickname) => (socket["nickname"] = nickname)); // 프론트에서 입력받은 nickname 인자를 socket 배열에 추가하고 업데이트함
 });
 
 const handleListen = () => console.log(`Listening on http:localhost:3000`);
 
 httpServer.listen(3000, handleListen);
-
-/*
-const wss = new WebSocket.Server({ server });
-
-const sockets = []; // 페이크 DB
-
-wss.on("connection", (socket) => {
-  sockets.push(socket); // 소켓을 sockets 에 푸쉬함
-  socket["nickname"] = "Anon";
-  console.log(socket);
-  console.log("Connected to Browser ✅"); // 브라우저와 연결되었을 때 출력
-  socket.on("close", () => console.log("Disconnectd from the Browser ❌")); //브라우저가 종료되면 출력
-  socket.on("message", async (message) => {
-    const messageString = message.toString("utf8"); // unicode로 나와서 utf8로 변환
-    const msgParsed = JSON.parse(messageString); // 프론트에서 오브젝트로 된 문자열을 받고 JSON.parse 기능으로 백앤드에서 사용하기 유용하게 다시 object로 변환시킨다
-    //console.log(msgParsed, messageString);
-    switch (msgParsed.type) {
-      case "new_message": // 메세지일때
-        sockets.forEach((aSocket) =>
-          aSocket.send(`${socket.nickname} : ${msgParsed.payload}`)
-        ); // sockets에 있는 wws를 각각에 전달함
-      case "nickname": // 닉네임일때
-        socket["nickname"] = msgParsed.payload;
-      //console.log(msgParsed.payload);
-    }
-
-    // 프론트앤드에서의 메세지를 입력받아서 출력함
-    //console.log(message);
-  });
-  //socket.send("Hello!"); // 백앤드에서 프론트앤드로 요청 후 출력함
-});
-
-
-*/
